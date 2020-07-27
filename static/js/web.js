dc3ee6987dd831ee5abbe4b868eaa5ed3703f4db
barbeque321 <adambbq@gmail.com>
2020-07-27 18:50:47 +0200

    new

--- static/js/web.js
+++ static/js/web.js
@@ -101,6 +101,7 @@ $(function(){
         L.marker([54.391091, 18.600883]).addTo(map).bindPopup('Some lazy Coder cave').openPopup();
         var popup = L.popup();
         console.log('submitting');
+        console.log(latLngs);
         $.ajax({
             type: "GET",
             url: 'process_loc/',
