b5ec75b5fb9c8dbc55f966d5f3b4e0c8a03b620b
barbeque321 <adambbq@gmail.com>
2020-07-27 16:48:43 +0200

    new

--- aitrust_map/views.py
+++ aitrust_map/views.py
@@ -6,7 +6,7 @@ def azure_map_project(request):
     return render(request, 'aitrust_map.html', {})
 
 def process_loc(request):
-    if (request.is_ajax) and (request.method == "GET"):
+    if request.method == "GET":
         latLngs = request.GET.get('latLngs')
     else:
         return JsonResponse({"error": ""}, status=400)
