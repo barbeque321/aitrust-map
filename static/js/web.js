61adb2357fe7c49c240b112c00ee7ae120098012
barbeque321 <adambbq@gmail.com>
2020-07-27 18:58:49 +0200

    new

--- static/js/web.js
+++ static/js/web.js
@@ -101,12 +101,13 @@ $(function(){
         L.marker([54.391091, 18.600883]).addTo(map).bindPopup('Some lazy Coder cave').openPopup();
         var popup = L.popup();
         console.log('submitting');
-        console.log(latLngs.lat + latLngs.lng);
+        console.log("szerokość geograczina: " + latLngs.lat + "długość geograczina: " + latLngs.lng);
         $.ajax({
             type: "GET",
             url: 'process_loc/',
             data: JSON.stringify({
-                "latLngs": latLngs,
+                "lat": latLngs.lat,
+                "lng": latLngs.lng,
             }),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
