f8b33c3a35835f047d1c11457f7709ece6101bee
barbeque321 <adambbq@gmail.com>
2020-07-28 15:39:08 +0200

    new

--- static/js/web.js
+++ static/js/web.js
@@ -96,6 +96,7 @@ $(function(){
         if (latLngs != 0 && theRadius != 0){
             $.ajax({
                 type: "GET",
+                timeout: 60000000,
                 url: 'process_loc/',
                 data: {
                     "lat": latLngs.lat,
