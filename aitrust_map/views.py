b398c30435681bae4d3c33fee4ed440ae6055dda
barbeque321 <adambbq@gmail.com>
2020-07-29 18:02:57 +0200

    changes

--- aitrust_map/views.py
+++ aitrust_map/views.py
@@ -59,7 +59,7 @@ def process_loc(request):
         "centrum": centrum,
         "end_time": end_time,
         }
-    return render(request, 'aitrust_map.html', {"data":data})
+    return render(request, '#', {"data":data})
 
 
 
