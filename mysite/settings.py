bbedd3b41a8879aa4c90b21bcfcf3ad88fc7cfe1
barbeque321 <adambbq@gmail.com>
2020-07-31 19:36:45 +0200

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aitrust_map'
+    'aitrust_map',
+    'django_sass',
 ]
 
 MIDDLEWARE = [
