6b9c86642ce54e425425f54d7c261694b2c314a5
barbeque321 <adambbq@gmail.com>
2020-08-05 15:44:33 +0200

    new

--- aitrust_map/views.py
+++ aitrust_map/views.py
@@ -95,6 +95,7 @@ def process_loc(request):
         query = """SELECT Id, kodPocztowy, Lat, Lng, ACOS(SIN(%(lat)s)*SIN(RADIANS(Lat)) + COS(%(lat)s)*COS(RADIANS(Lat))*COS(RADIANS(Lng)-%(lng)s))*%(R)s AS D FROM (SELECT Id, kodPocztowy, Lat, Lng FROM pomorskie WHERE Lat BETWEEN %(minLat)s AND %(maxLat)s AND Lng BETWEEN %(minLng)s AND %(maxLng)s) AS FirstCut WHERE ACOS(SIN(%(lat)s)*SIN(RADIANS(Lat)) + COS(%(lat)s)*COS(RADIANS(Lat))*COS(RADIANS(Lng)-%(lng)s))*%(R)s < %(rad)s ORDER BY D;"""
         cursor.execute(query, params)
         data = cursor.fetchall()
+        print(data)
 
 
     return JsonResponse(data, safe=False)
