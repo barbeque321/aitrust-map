c1aad779cae5b8920f152c91ea660f09ace5f7fc
Adam Hirschfeld <adamhirschfeld@MacBook-Pro-Adam-2.local>
2020-08-08 18:11:11 +0200

    new

--- static/js/web.js
+++ static/js/web.js
@@ -158,8 +158,7 @@ $(function(){
                         document.getElementById("info_adress_sum_10").innerHTML = data.points_sum_up_10;
                         document.getElementById("info_postal_code_sum_difference").innerHTML = " (+" + data.difference_postal_num + ")";
                         document.getElementById("info_adress_sum_difference").innerHTML = " (+" + data.differene_points_num + ")";
-                        clamp(document.getElementById('postal_code'), 3).trigger(clamp_event, detail);
-                        document.dispatchEvent(clamp_event);
+                        clamp(document.getElementById('postal_code'), 3);
                     }, 
                     error: function (jqXhr, textStatus, errorThrown) {
                         $('#loadingmessage').hide();
