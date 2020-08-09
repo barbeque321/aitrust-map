e515d365f7b1149bca7f0debd9338278eb2cea25
Adam Hirschfeld <adamhirschfeld@MacBook-Pro-Adam-2.local>
2020-08-09 19:10:34 +0200

    new

--- static/js/web.js
+++ static/js/web.js
@@ -150,7 +150,7 @@ $(function(){
                         $('#postal_code_sum').contents()[0].textContent = data.postal_code_sum;
                         postal_list_to_draw = data.postal_code;
                         document.getElementById("postal_code").innerHTML = data.postal_code;
-                        console.log(data.postal_code);
+                        console.log(data.postal_list_no_repeats);
                         document.getElementById("postal_codes_popupbox").innerHTML = data.postal_code;
                         document.getElementById("info_radius").innerHTML = data.rad + "km";
                         document.getElementById("info_postal_code_sum").innerHTML = data.postal_code_sum;
@@ -509,6 +509,7 @@ $(function(){
                         $('#loadingmessage').hide();
                         console.log("Ready");
                         console.log(data.process_data);
+                        console.log(data.postal_string_with_quote);
                         let polygon = data.point_list;
                         // swaping places the lat with the lng
                         let arr = [];
